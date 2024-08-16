bc0478ea6acfa220df9d9b885a1907bc3e8a6498
aadityaagra <aadityaagrawal24@gmail.com>
2024-08-16 19:36:27 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,77 @@
+# # server.py
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO, emit
+# import cv2
+# import base64
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @socketio.on('video_frame')
+# def handle_video_frame(data):
+#     # Broadcast the received frame to all connected clients
+#     emit('broadcast_frame', data, broadcast=True)
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+
+
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO, emit
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @socketio.on('new_user')
+# def handle_new_user(data):
+#     # Broadcast the new user's box color to all connected clients
+#     emit('new_user_box', data, broadcast=True)
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO, emit
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @socketio.on('new_user')
+# def handle_new_user(data):
+#     # Broadcast the new user's image to all connected clients
+#     emit('new_user_image', data, broadcast=True)
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('image_capture')
+def handle_image_capture(data):
+    # Broadcast the received image to all connected clients
+    emit('broadcast_image', data, broadcast=True)
+
+if any(platform.win32_ver()):
+    socketio.run(app, debug=True)
