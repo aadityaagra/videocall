ef14dfc29b4e707c3c12be50042b6954de467e5a
aadityaagra <aadityaagrawal24@gmail.com>
2024-08-17 14:56:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
